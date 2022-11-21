e33330824752bdcfb5e175d1c6748740ac672a61
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-11-21 03:18:59 +0200

    solve deployment issue

--- db/migrate/20221110081607_create_rents.rb
+++ db/migrate/20221110081607_create_rents.rb
@@ -1,8 +1,8 @@
 class CreateRents < ActiveRecord::Migration[7.0]
   def change
     create_table :rents do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :tractor, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true, on_delete: :cascade
+      t.references :tractor, null: false, foreign_key: true, on_delete: :cascade
       t.integer :total_costs
       t.date :rent_date
 
